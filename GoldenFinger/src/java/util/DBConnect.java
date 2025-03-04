d08d4c5b51b5456d4fd08c378453413c1e70e657
kl3inIT <dathip04@gmail.com>
2025-03-04 22:54:41 +0700

    test

--- GoldenFinger/src/java/util/DBConnect.java
+++ GoldenFinger/src/java/util/DBConnect.java
@@ -14,8 +14,8 @@ public class DBConnect {
     public DBConnect() {
         try {
             String user = "sa";
-            String password = "123456";
-            String url = "jdbc:sqlserver://LAPTOP-3EKS6U5D\\SQLEXPRESS:1433;databaseName=GoldenFinger";
+            String password = "Dat123456";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=GoldenFinger";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = (Connection) DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
